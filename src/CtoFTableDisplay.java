7dc6ed3b62f328e951203c41d7470c7b637754e4
AndrewGWMHS <goff.614615@swl.k12.oh.us>
2024-11-06 08:50:06 -0500

    2

--- src/CtoFTableDisplay.java
+++ src/CtoFTableDisplay.java
@@ -1,4 +1,22 @@
-package PACKAGE_NAME;
-
 public class CtoFTableDisplay {
+    public static void main(String[] args) {
+        System.out.println("Celsius  Fahrenheit");
+
+        for (int i = -100; i <= 100; i++) {
+            double f = CtoF(i);
+            System.out.printf("%-5d %10.2f \n", i, f);
+         }
+
+
+
+
+
+
+    }
+
+    public static double CtoF(double Celsius) {
+        double c = Celsius;
+        double f = (c*1.8) + 32;
+        return f;
+    }
 }
